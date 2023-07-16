b5118f47110faea73d676ba044284b5ffd9b1ba6
Marcos Felipe <mf7778838@gmail.com>
2023-07-15 21:16:19 -0300

    Obtendo um usuário por id

--- src/main/java/com/marcos/workshopmongo/services/UserService.java
+++ src/main/java/com/marcos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marcos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcos.workshopmongo.entities.User;
 import com.marcos.workshopmongo.repositories.UserRepository;
+import com.marcos.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,8 +20,8 @@ public class UserService {
 		return repository.findAll();
 	}
 	
-//	public User findById(String id) {
-//		Optional<User> obj = repository.findById(id);
-//		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-//	}
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
